7d138a1f8950afa2db25426e57e2e76294278ef1
viniciusyashuda <viniciusyashuda008@gmail.com>
2022-03-08 09:46:36 -0300

    Add project files.

---
+++ MutiplicationTable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int result;
+
+            for (int i = 0; i < 11; i++)
+            {
+
+                for (int j = 0; j < 11; j++)
+                {
+
+                    result = i * j;
+                    Console.WriteLine(i + " * " + j + " = " + result);
+
+                }
+
+                Console.WriteLine("\n");
+                Console.ReadKey();
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
